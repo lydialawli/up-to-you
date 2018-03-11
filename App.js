94022f9a0c83e2ce4e4e78ef41fde7c08392500f
Lydia Law <lydialawli@gmail.com>
2018-03-11 20:04:48 +0100

    testing bg color

--- App.js
+++ App.js
@@ -19,7 +19,12 @@ export default class App extends React.Component {
   render() {
 
     return (
-      <View style={styles.container}>
+      <View style={{
+        flex: 1,
+      }}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
 
         <Button
           title= {this.state.quote}
