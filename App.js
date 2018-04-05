0178bd89c43cf8988af534c5d9fa68ec6baedebf
Lydia Law <lydialawli@gmail.com>
2018-04-05 11:02:26 +0200

    deleting useless things from previous app

--- App.js
+++ App.js
@@ -1,37 +1,21 @@
 import React from 'react'
 import { StyleSheet, Text, View, Button, Images} from 'react-native'
-import QuoteFactory from './src/QuoteFactory.js' 
-import PresentationalComponent from './src/PresentationalComponent.js'
 import ImagesExamples from './src/ImagesExamples.js'
-
 export default class App extends React.Component {
 
-  constructor(){
-    super()
-    this.state = {
-      quote : "click me if you want to be wiser"
-    }
-  }
 
-  onPressed=()=>{
-    let newquote = QuoteFactory.getRandomQuote()
-    this.setState({quote:newquote}) 
-  }
 
   render() {
 
     return (
-      <View style = {styles.container}>    
-        <PresentationalComponent
-          quote = {this.state.quote} 
-          onPressed = {this.onPressed}/>
+      <View style = {styles.container}>     
         <ImagesExamples/>
       </View>
     )
   }
 }
 
-const uniPerson = () => {
+const coin = () => {
   return (
      <ImagesExamples/>
   )
@@ -39,7 +23,7 @@ const uniPerson = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0,
     backgroundColor: 'deepskyblue',
     alignItems: 'center',
     justifyContent: 'center',
