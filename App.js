ec3be828371f966b79160aa48a6d738d9550d3d9
Lydia Law <lydialawli@gmail.com>
2018-04-05 14:27:30 +0200

    Using new component

--- App.js
+++ App.js
@@ -1,25 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button, Images} from 'react-native'
-import ImagesExamples from './src/ImagesExamples.js'
+import { StyleSheet, Text, View, Button, Image} from 'react-native'
+import Coin from './src/coin.js'
+
 export default class App extends React.Component {
-
-  state = {
-    coin: <ImagesExamples/>
-
-  }
 
   render() {
 
     return (
       <View style = {styles.container}>    
-        {this.state.coin}
-        
+        <Coin value = {Math.floor(Math.random())} />
       </View>
     )
   }
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
