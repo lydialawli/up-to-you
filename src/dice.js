45fcccfdc6bedd9ff98011f649badba4ef3f4751
Lydia Law <lydialawli@gmail.com>
2018-04-06 20:11:09 +0200

    removing dice images from dice.js to app.js

--- src/dice.js
+++ src/dice.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import { StyleSheet, Text, View, TouchableWithoutFeedback, Image} from 'react-native'
 
-var diceSides = [
-    require('./img/dice-1.png'),
-    require('./img/dice-2.png'),
-    require('./img/dice-3.png'),
-    require('./img/dice-4.png'),
-    require('./img/dice-5.png'),
-    require('./img/dice-6.png')
-]
+
 
 export default class Dice extends React.Component {
 
     
     getImageByValue = (value) =>
     {
-        return <Image style = {{width:200, height:200}} source = {diceSides[value]} />
+        return <Image style = {{width:200, height:200}} source = {this.props.options[value]} />
     }
     
     render() {
