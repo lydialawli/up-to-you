846b0e2fb817fcce6689c8df0a3f410d2f901d0e
Lydia Law <lydialawli@gmail.com>
2018-04-06 19:29:41 +0200

    corrected image that returns

--- src/dice.js
+++ src/dice.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { StyleSheet, Text, View, TouchableWithoutFeedback, Image} from 'react-native'
 
 var diceSides = [
-    {image:require('./img/dice-1.png"')},
-    {image:require('./img/dice-2.png"')},
-    {image:require('./img/dice-3.png"')},
-    {image:require('./img/dice-4.png"')},
-    {image:require('./img/dice-5.png"')},
-    {image:require('./img/dice-6.png"')}
+    require('./img/dice-1.png'),
+    require('./img/dice-2.png'),
+    require('./img/dice-3.png'),
+    require('./img/dice-4.png'),
+    require('./img/dice-5.png'),
+    require('./img/dice-6.png')
 ]
 
 export default class Dice extends React.Component {
@@ -15,15 +15,15 @@ export default class Dice extends React.Component {
     
     getImageByValue = (value) =>
     {
-        return diceSides[value]
+        return <Image style = {{width:200, height:200}} source = {diceSides[value]} />
     }
     
     render() {
         
         return (
-        <TouchableWithoutFeedback onPress= {this.props.onPress}>    
-           {this.getImageByValue(this.props.value)}
-        </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress= {this.props.onPress}>    
+                {this.getImageByValue(this.props.value)}
+            </TouchableWithoutFeedback>
         )
     }
 }
