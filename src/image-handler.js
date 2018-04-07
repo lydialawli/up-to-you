3795ef08777999969f47b9e6093f15c7cccdce27
Lydia Law <lydialawli@gmail.com>
2018-04-07 20:31:59 +0200

    created image handler

---
+++ src/image-handler.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Image} from 'react-native'
+
+
+export default class imageHandler extends React.Component {
+
+    
+    getImageByValue = (value) =>
+    {
+        return <Image style = {{width:200, height:200}} source = {this.props.options[value]} />
+    }
+    
+    render() {
+        
+        return (
+            <TouchableWithoutFeedback onPress= {this.props.onPress}>    
+                {this.getImageByValue(this.props.value)}
+            </TouchableWithoutFeedback>
+        )
+    }
+}
